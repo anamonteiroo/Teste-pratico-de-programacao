ad928120979eb9ed090a1341f4b62a21161e1173
Ana Monteiro <78883582+anamonteiroo@users.noreply.github.com>
2021-03-17 19:53:48 -0300

    teste1

--- Teste Smarkio/index.js
+++ Teste Smarkio/index.js
@@ -56,6 +56,8 @@ app.post('/add', function(req, res) {
     });
 })
 
-app.listen(3030, function() {
-    console.log("Servidor rodando na url http://localhost:3030")
+
+
+app.listen(8081, function() {
+    console.log("Servidor rodando na url http://localhost:8081")
 })
